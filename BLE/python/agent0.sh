7c12fef1660106f342b294f053aa5630a61c55b1
arao <araobp@users.noreply.github.com>
2019-04-16 17:56:01 +0900

    update

---
+++ BLE/python/agent0.sh
@@ -0,0 +1,2 @@
+sudo python3 ./agent.py -i 0 -d RN079D -s 11223344-5566-7788-9900-aabbccddeeff -c 01020304-0506-0708-0900-0a0b0c0d0e0f
+
---
+++ BLE/python/agent2.sh
@@ -0,0 +1,3 @@
+# BLE simulator on Android
+sudo python3 ./agent.py -i 2 -d Battery -s 180f -c 2a19 
+
