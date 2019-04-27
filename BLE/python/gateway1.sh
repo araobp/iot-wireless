e65abe519fc5c3be83d492a63ce808e65b5ca524
arao <araobp@users.noreply.github.com>
2019-04-27 17:59:03 +0900

    update

--- BLE/python/gateway1.sh
+++ BLE/python/gateway1.sh
@@ -1,3 +1,3 @@
-# connect BLE simulator on Android via hci1
-sudo python3 ./gateway.py -i 1 -d Battery -s 180f -t 2a19 
+# hci1
+sudo python3 ./gateway.py -i 1 -d BLE1 -s 11223344-5566-7788-9900-aabbccddeeff -r 01020304-0506-0708-0900-0a0b0c0d0e0f -t 01020304-0506-0708-0900-0a0b0c0deeff
 
--- BLE/python/gateway2.sh
+++
@@ -1,3 +0,0 @@
-# connect BLE simulator on Android via hci2
-sudo python3 ./gateway.py -i 2 -d Battery -s 180f -t 2a19 
-
